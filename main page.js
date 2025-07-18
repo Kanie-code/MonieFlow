a00a889a78b5ab52bffd48958f1fbda36fd85df8
Kanie <211605070+Kanie-code@users.noreply.github.com>
2025-07-18 18:35:18 +0100

    refining

--- main page.js
+++ main page.js
@@ -1,11 +1,31 @@
 document.addEventListener('DOMContentLoaded' , () => {
     const balance = document.getElementById('balance');
+    const balanceShow = document.getElementById('balanceShow');
     const welcomeMessage = document.getElementById('welcomeMessage');
+    const balanceStars = document.getElementById('balanceStars');
     const img = document.getElementById('img');
     let logedInProfile = JSON.parse(localStorage.getItem('target-profile'));
 
     img.src = logedInProfile.pImage;
     welcomeMessage.textContent += ` ${logedInProfile.pFirstName},`
     balance.textContent += logedInProfile.pBalance;
+
+    balanceShow.addEventListener('click', () => hideBalance());
+
+
+    balanceShow.style.cursor = 'pointer'
+
+
+
+    function hideBalance () {
+        if (balance.classList.contains('hide')) {
+            balance.classList.remove('hide');
+            balanceStars.classList.add('hide');
+        }
+        else {
+            balanceStars.classList.remove('hide')
+            balance.classList.add('hide');
+        }
+    }
 
 });
